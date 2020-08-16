7ba4873dba1dd1ca81585b9080b456c18dba5abb
ldelbel <lucdelbel@gmail.com>
2020-08-16 12:16:11 -0300

    Add editing resource to users, error tracking in edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
+  
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
     @user = User.find(params[:id])
     @user.update(user_params)
   end
